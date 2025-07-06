2aa926c1a703634295d9f88fc202ae001359accf
Kishan_Chauhan <kishanchauhan2006.25@gmail.com>
2025-07-06 16:12:35 +0530

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hello_app/views/home_view.dart';
+
+//stateless Widget is a widget that does not requires mutable state
+//stateless means the state is not going to be change it will be constant
+//statefull Widget is a widget that requires mutable state
+//may change it's state upon any event like click event
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "flutter_learn",
+      home: HomeView()
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hello_app/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
