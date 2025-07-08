d70896951c75bda3d6814babc4b70fe757c5ffd4
Kishan_Chauhan <kishanchauhan2006.25@gmail.com>
2025-07-08 21:49:22 +0530

    Calculator app

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -5,22 +5,42 @@ class CalculatorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        //Calculator display
-        TextField(
-          decoration: InputDecoration(
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
-            ),
-            hintText: "Enter a number",
-            hintStyle: TextStyle(color: Colors.white),
-          ),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        children: [
+          //Calculator display
+          DisplayOne(hint: "Enter First Number"),
+          SizedBox(height: 30),
+          DisplayOne(hint: "Enter Second Number"),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
 
-        //Expand
-        //Calculator Buttons
-      ],
+class DisplayOne extends StatelessWidget {
+  final String? hint;
+  const DisplayOne({super.key, this.hint = "Enter a number"});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      autofocus: true,
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black, width: 3.0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black, width: 3.0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.black),
+      ),
     );
   }
 }
